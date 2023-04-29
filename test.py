d2ce62c73156792d770817473e488764d04271a0
ehrharoska <ehrharoska@vuw.ac.nz>
2023-04-29 22:36:16 +1200

    testing with 6 species

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 import backend
 
-testBE = backend.QuizBuilder(['magpet1', 'snopet1'], 5)
+testBE = backend.QuizBuilder(['magpet1', 'snopet1', 'nezkak1', 'kea1', 'gagcoc1', 'shoebi1'], 5)
 print([(q.imageURL, q.speccode, q.species_options) for q in testBE.questions])
