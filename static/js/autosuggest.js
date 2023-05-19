5c2400350f555327d8b68338692b5a0092bff21c
Skadoink <oskaege@gmail.com>
2023-05-19 22:48:50 +1200

    fuzzysort and beginning of multi-species adding

--- static/js/autosuggest.js
+++ static/js/autosuggest.js
@@ -28,8 +28,9 @@ function populateSpeciesList(speciesList) {
   });
   
   speciesInput.addEventListener('input', function() {
-    const inputValue = this.value.toLowerCase();
-    const filteredOptions = speciesList.filter(species => species.toLowerCase().startsWith(inputValue));
+    const inputValue = this.value;
+    const fuzzyResults = fuzzysort.go(inputValue, speciesList);
+    const filteredOptions = fuzzyResults.map(result => result.target);
     dataList.innerHTML = '';
     filteredOptions.forEach(option => {
       const newOption = document.createElement('option');
@@ -38,3 +39,39 @@ function populateSpeciesList(speciesList) {
     });
   });
 }
+
+// Define an array to store the selected species
+let selectedSpecies = [];
+
+// Function to add a species to the selectedSpecies array and display it
+function addSpecies(species) {
+    selectedSpecies.push(species);
+    const selectedSpeciesDiv = document.getElementById('selectedSpecies');
+    const speciesItem = document.createElement('div');
+    speciesItem.textContent = species;
+    selectedSpeciesDiv.appendChild(speciesItem);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Rest of your existing code here...
+
+    // Add event listener to the form submission
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        // Get the number of questions and perform further actions...
+        const numQuestionsInput = document.getElementById('num_questions');
+        const numQuestions = parseInt(numQuestionsInput.value);
+        // Rest of your logic to handle quiz start and species selection...
+    });
+
+    // Event listener for species selection
+    const speciesInput = document.getElementById('speciesInput');
+    speciesInput.addEventListener('change', function() {
+        const selectedSpecies = speciesInput.value;
+        if (selectedSpecies.trim() !== '') {
+            addSpecies(selectedSpecies);
+            speciesInput.value = '';
+        }
+    });
+});
