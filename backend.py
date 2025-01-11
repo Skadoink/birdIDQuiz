5eed7594c51e6f57263e9f351978fe6da6ab3996
Oskar <63914721+Skadoink@users.noreply.github.com>
2025-01-11 23:08:24 +1300

    python formatting

--- backend.py
+++ backend.py
@@ -6,6 +6,7 @@ import random
 import urllib.request
 from bs4 import BeautifulSoup
 
+
 class Question:
     def __init__(self, speccode, image_URL, species_options):
         """
@@ -17,20 +18,25 @@ class Question:
         self.speccode = speccode
         self.image_URL = image_URL
         self.species_options = species_options
-        self.spec_name = [d['species_name'] for d in species_options if d['species_code'] == speccode][0] #get the name of the species with the correct code
+        self.spec_name = [
+            d["species_name"] for d in species_options if d["species_code"] == speccode
+        ][
+            0
+        ]  # get the name of the species with the correct code
         self.embed_URL = self.get_embed_URL(image_URL)
-        
+
     def get_embed_URL(self, image_URL):
         """
         Gets the embed URL for the image.
         @param image_URL: URL of the image
         """
         image_ID = image_URL.split("/")[-2]
-        embed_URL = "https://macaulaylibrary.org/asset/"+image_ID+"/embed" 
+        embed_URL = "https://macaulaylibrary.org/asset/" + image_ID + "/embed"
         # embed_URL = '<iframe src="https://macaulaylibrary.org/asset/'+image_ID+'/embed" height="507" width="640" frameborder="0" allowfullscreen></iframe>'
         return embed_URL
-            
-class QuizBuilder: 
+
+
+class QuizBuilder:
     def __init__(self, species_names, num_questions):
         """
         Makes a quiz with the given species and number of questions.
@@ -51,13 +57,13 @@ class QuizBuilder:
         """
         Makes a dictionary of species codes and names.
         """
-        species_csv = csv.reader(open('static/species2024.csv', 'r'))
-        species_codes = [] #list of species codes
-        species = [] #list of dictionaries with species code and common name
+        species_csv = csv.reader(open("static/species2024.csv", "r"))
+        species_codes = []  # list of species codes
+        species = []  # list of dictionaries with species code and common name
         for row in species_csv:
             if row[1] in self.species_names:
                 species_codes.append(row[0])
-                species.append({"species_code": row[0],"species_name": row[1]})
+                species.append({"species_code": row[0], "species_name": row[1]})
             if len(self.species_names) == len(species):
                 break
         return species, species_codes
@@ -70,36 +76,55 @@ class QuizBuilder:
         species_path = "species_CSVs"
         if not os.path.exists(species_path):
             os.makedirs(species_path)
-        onlyfiles = [f for f in os.listdir(species_path) if isfile(join(species_path, f)) and f.endswith('.csv')]
+        onlyfiles = [
+            f
+            for f in os.listdir(species_path)
+            if isfile(join(species_path, f)) and f.endswith(".csv")
+        ]
         for spec in self.species_codes:
             if len(onlyfiles) == 0:
                 self.updateCSV(spec)
             for i, file in enumerate(onlyfiles):
-                #correct species and up to date
+                # correct species and up to date
                 if spec in file and str(currentYearMonth) in file:
                     break
-                #correct species but not up to date
+                # correct species but not up to date
                 elif spec in file and str(currentYearMonth) not in file:
                     self.updateCSV(spec)
-                    os.remove(join(species_path, file)) #TODO should only do this if the update is successful
-                #if we've reached the end of the list and haven't found a match
-                elif i == len(onlyfiles) - 1: 
-                    self.updateCSV(spec) 
-                
+                    os.remove(
+                        join(species_path, file)
+                    )  # TODO should only do this if the update is successful
+                # if we've reached the end of the list and haven't found a match
+                elif i == len(onlyfiles) - 1:
+                    self.updateCSV(spec)
+
     def updateCSV(self, spec):
         """
         Gets the latest image URLs from the species' media page on eBird and saves it to a CSV file.
         @param spec: species code
-        """    
-        url = "https://media.ebird.org/catalog?taxonCode="+spec+"&sort=rating_rank_desc&mediaType=photo&view=list"
+        """
+        url = (
+            "https://media.ebird.org/catalog?taxonCode="
+            + spec
+            + "&sort=rating_rank_desc&mediaType=photo&view=list"
+        )
         html_page = urllib.request.urlopen(url)
         soup = BeautifulSoup(html_page, "html.parser")
         images = []
-        for img in soup.findAll('img'):
-            src = img.get('src')
-            if src.startswith("https://cdn.download.ams.birds.cornell.edu/api/v2/asset/"):
+        for img in soup.findAll("img"):
+            src = img.get("src")
+            if src.startswith(
+                "https://cdn.download.ams.birds.cornell.edu/api/v2/asset/"
+            ):
                 images.append(src)
-        with open(join("species_CSVs", spec + "_" + datetime.datetime.now().strftime("%Y-%m") + ".csv"), 'w', newline='') as f:
+        with open(
+            join(
+                "species_CSVs",
+                spec + "_" + datetime.datetime.now().strftime("%Y-%m") + ".csv",
+            ),
+            "w",
+            newline="",
+        ) as f:
             writer = csv.writer(f)
             for image in images:
                 writer.writerow([image])
@@ -124,7 +149,7 @@ class QuizBuilder:
         for file in os.listdir("species_CSVs"):
             print(file, speccode)
             if speccode in file:
-                with open(join("species_CSVs", file), 'r') as f:
+                with open(join("species_CSVs", file), "r") as f:
                     reader = csv.reader(f)
                     data = list(reader)
                     imageURL = random.choice(data)[0]
--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 """
 For testing the quizbuilder
 """
+
 import backend
 
-testBE = backend.QuizBuilder(['magpet1', 'snopet1', 'nezkak1', 'kea1', 'gagcoc1', 'shoebi1'], 5)
+testBE = backend.QuizBuilder(
+    ["magpet1", "snopet1", "nezkak1", "kea1", "gagcoc1", "shoebi1"], 5
+)
 print([(q.imageURL, q.speccode, q.species_options) for q in testBE.questions])
 
 """
